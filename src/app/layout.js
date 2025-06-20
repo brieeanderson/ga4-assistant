73238b1fc24a67ccf64a753f9aa53f40ba658c48
brieeanderson <beastanalyticsco@gmail.com>
2025-06-20 09:24:20 -0500

    Create layout.js

---
+++ src/app/layout.js
@@ -0,0 +1,12 @@
+export const metadata = {
+  title: 'GA4 Helper - Coming Soon',
+  description: 'Stop losing money on broken GA4 setup',
+}
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body>{children}</body>
+    </html>
+  )
+}
