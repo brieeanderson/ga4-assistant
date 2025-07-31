ba10741b8ceb19a16ff0e269e209cd56c8985305
Brianna Anderson <brie@beastanalyticsco.com>
2025-07-31 15:10:51 -0500

    Add Google Tag Manager (GTM-WKVD32JW) to the site
    - Add GTM script to head section using dangerouslySetInnerHTML
    - Add GTM noscript fallback to body for users with JavaScript disabled
    - Properly formatted for Next.js with React syntax
    - GTM container ID: GTM-WKVD32JW
    - Follows Google's recommended implementation pattern

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -57,8 +57,31 @@ export default function RootLayout({
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link rel="dns-prefetch" href="//fonts.googleapis.com" />
         <link rel="dns-prefetch" href="//fonts.gstatic.com" />
+        
+        {/* Google Tag Manager */}
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-WKVD32JW');`
+          }}
+        />
+        {/* End Google Tag Manager */}
       </head>
       <body className={`${inter.variable} ${bebasNeue.variable} antialiased bg-gray-950 text-white`}>
+        {/* Google Tag Manager (noscript) */}
+        <noscript>
+          <iframe 
+            src="https://www.googletagmanager.com/ns.html?id=GTM-WKVD32JW"
+            height="0" 
+            width="0" 
+            style={{display:'none',visibility:'hidden'}}
+          />
+        </noscript>
+        {/* End Google Tag Manager (noscript) */}
+        
         {children}
       </body>
     </html>
